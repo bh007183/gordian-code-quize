877ce64fa55afa08626bdf6e382e18fad460b8a1
Ben Hopkins <bjhops17@gmail.com>
2021-03-28 11:28:29 -0700

    stuck....cant figure out how to get it to nest the way i want....

--- seatmap_parser.py
+++ seatmap_parser.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+
 import json
 tree = ET.parse('seatmap1.xml')
 root = tree.getroot()
@@ -6,20 +7,59 @@ root = tree.getroot()
 dict = {}
 
 
-for elem in tree.iter():
-    if len(elem) > 0:
-        lastChar = str(elem)[-20:]
-        print(lastChar)
-        item = str(elem).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "")
-        dict[item] = {"element": list(str(elem.iter()).join(" ")), "attributes": elem.attrib}
-    if len(elem) == 0:
-        item = str(elem).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "")
-        dict[item] = {"attributes": elem.attrib}
+# for level1 in tree.iter():
+print(value(root))
+    # if len(level1) > 0:
+    #     # lastChar = str(level1)[-20:]
+    #     item = str(level1).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "")
+    #
+    #     elmarray = list(level1)
+    #     # basearr = [str(i).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "") for i in elmarray]
+    #     dict[item] = {"Elements": elmarray, "attributes": level1.attrib}
 
-
-# print(ET.tostring(root, encoding='utf8').decode('utf8'))
-#  element.attrib
 #
-j_data = json.dump(dict, open("FILENAME_parsed.json", 'w'))
+# for i in range(len(test_list)):
+#     print (i, end = " ")
+#     print (test_list[i])
+#
+#         #
+        # for level2 in elmarray:
+        #     if len(level2) > 0:
+            #     lastChar = str(level2)[-20:]
+            #     item = str(level2).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "")
+            #     elmarray = list(level2)
+            #     fixedarr = [str(i).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "") for i in elmarray]
+            #     dict[item] = {"Elements": fixedarr, "attributes": level2.attrib}
+        #         for level3 in level2:
+        #             if len(level3) > 0:
+        #                 for level4 in level3:
+        #                     if len(level4) > 0:
+        #                         for level5 in level4:
+        #                             if len(level5) > 0:
+        #                                 for level6 in level5:
+        #                                     print(level6)
+        #
+        #                             else:
+        #                                 lastChar = str(level5)[-20:]
+        #                                 item = str(level5).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "")
+        #                                 dict[item] = {"text": level5.text, "attributes": level5.attrib}
+        #                     else:
+        #                         lastChar = str(level4)[-20:]
+        #                         item = str(level4).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "")
+        #                         dict[item] = {"text": level4.text, "attributes": level4.attrib}
+        #             else:
+        #                 lastChar = str(level3)[-20:]
+        #                 item = str(level3).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "")
+        #                 dict[item] = {"text": level3.text, "attributes": level3.attrib}
+            # else:
+            #     lastChar = str(level2)[-20:]
+            #     item = str(level2).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "")
+            #     dict[item] = {"text": level2.text, "attributes": level2.attrib}
 
-# print(dict)
+
+    # else:
+    #     lastChar = str(level1)[-20:]
+    #     item = str(level1).replace("<Element '{http://www.opentravel.org/OTA/2003/05/common/}", "")
+    #     dict[item] = {"text": level1.text, "attributes": level1.attrib}
+
+# j_data = json.dump(dict, open("FILENAME_parsed.json", 'w'))
